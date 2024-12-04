c518d17ea54c6dad8d660b3cdb28d7217345471f
Rahkem <rahkemfleming@gmail.com>
2024-12-04 05:34:48 -0700

    Updated Port

--- app.py
+++ app.py
@@ -164,4 +164,5 @@ def legal_moves(data):
 
 # Main entry point
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from the environment or default to 5000
+    socketio.run(app, host='127.0.0.1', port=port, debug=True)
